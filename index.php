4b3d7becfd418455c41e98b2c137a3f28d4d546b
xXReCreatorXx <133557944+xXReCreatorXx@users.noreply.github.com>
2024-09-16 13:23:44 +0700

    Edit index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo "Hello world"
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Kiriakow Alekcander">
+        <meta name="description" content="Just PHP and HTML">
+        <meta name="keywords" content="PHP">
+        <title>PHP</title>
+    </head>
+
+    <body>
+        <?php
+        echo "Hello world"
+        ?>  
+    </body>
+</html>
